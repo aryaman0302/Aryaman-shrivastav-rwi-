1954912389230a50115518f74612fed4c0a87865
aryaman0302 <aryamanshrivastav99@gmail.com>
2024-05-14 19:44:50 +0530

    frist commit

---
+++ src/AnagramCheck.java
@@ -0,0 +1,43 @@
+
+public class AnagramCheck {
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+
+        if (areAnagrams(str1, str2)) {
+            System.out.println("'" + str1 + "' and '" + str2 + "' are anagrams.");
+        } else {
+            System.out.println("'" + str1 + "' and '" + str2 + "' are not anagrams.");
+        }
+    }
+
+    public static boolean areAnagrams(String str1, String str2) {
+
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+
+        int[] count = new int[256];
+
+
+        for (int i = 0; i < str1.length(); i++) {
+            count[str1.charAt(i)]++;
+        }
+
+
+        for (int i = 0; i < str2.length(); i++) {
+            count[str2.charAt(i)]--;
+        }
+
+
+
+        for (int i = 0; i < 256; i++) {
+            if (count[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
